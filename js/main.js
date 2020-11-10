768e39212f05dac1df941266d158d8b3c51bd99f
Boyul Kim <boyulkim1@gmail.com>
2020-11-09 22:28:21 -0800

    completed player profile page - issue 1

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-var xml = null;
-function ballProjections() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://cors-anywhere.herokuapp.com/https://www.fantasybasketballnerd.com/service/draft-projections');
-  xhr.responseType = 'document';
-  xhr.addEventListener('load', function () {
-    console.log(xhr.status);
-    console.log(xhr.response);
-    xml = xhr.response;
-    console.log('xml', xml);
-    var jsonString = JSON.stringify(xmlToJson(xml));
-    console.log('to JSON', JSON.parse(jsonString));
-  });
-  xhr.send();
-}
-ballProjections();
+// var xml = null;
+// function ballProjections() {
+//   var xhr = new XMLHttpRequest();
+//   xhr.open('GET', 'https://cors-anywhere.herokuapp.com/https://www.fantasybasketballnerd.com/service/draft-projections');
+//   xhr.responseType = 'document';
+//   xhr.addEventListener('load', function () {
+//     console.log(xhr.status);
+//     console.log(xhr.response);
+//     xml = xhr.response;
+//     console.log('xml', xml);
+//     var jsonString = JSON.stringify(xmlToJson(xml));
+//     console.log('to JSON', JSON.parse(jsonString));
+//   });
+//   xhr.send();
+// }
+// ballProjections();
